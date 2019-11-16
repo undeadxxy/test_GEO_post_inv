a996a1ddf6411b83d64f0f42ddb92ab4ec89b183
stepbystep88 <bin.stepbystep@gmail.com>
2019-11-16 14:37:13 +0800

    update methods

--- testCommonCode.m
+++ testCommonCode.m
@@ -13,9 +13,6 @@ GPlotParam.linewidth = 1.5;
 GPlotParam.fontsize = 9;
 GPlotParam.fontweight = 'bold';
 
-GShowProfileParam.isLegend = 0;
-GShowProfileParam.plotParam = GPlotParam;
-
 GSegyInfo.isNegative = 0;
 basePath = 'D:/data/matlab_data/2018_POST_DLSR/geoeast';
 
@@ -27,7 +24,10 @@ load wellLogs;
 load timeLine.mat;
 load wavelet;
 
-% segy information of initial model 
+%% segy information of initial model 
+GPostInvParam.initModel.initLog = [];
+GPostInvParam.initModel.filtCoef = 0.4;
+% GPostInvParam.initModel.mode = 'filter_from_true_log';
 GPostInvParam.initModel.mode = 'segy';
 GPostInvParam.initModel.segyInfo = GSegyInfo;
 GPostInvParam.initModel.segyInfo.isPosZero = 0;
@@ -35,20 +35,23 @@ GPostInvParam.initModel.segyInfo.isPosZero = 0;
 GPostInvParam.initModel.segyInfo.t0 = 0;
 GPostInvParam.initModel.segyInfo.inlineId = 3;
 GPostInvParam.postSeisData.segyInfo.crosslineId = 6;
-% GPostInvParam.initModel.segyFileName = 'C:\Users\binst\HRS projects\GEO_POST\model\inv_multi_wells_for_init_IP_Horizon_init_005_onewell.sgy';
+GPostInvParam.initModel.segyFileName = 'C:\Users\binst\HRS projects\GEO_POST\model\inv_multi_wells_for_init_IP_Horizon_init_005_onewell.sgy';
 
 % GPostInvParam.initModel.segyInfo.t0 = 755;
 % GPostInvParam.initModel.segyFileName = 'D:\data\seismic data\geo-block\data\model\Imp_Lu_init_AM50Hz.sgy';
 
 % GPostInvParam.initModel.segyInfo.inlineId = 48;
-% GPostInvParam.postSeisData.segyInfo.crosslineId = 6;
+% GPostInvParam.initModel.segyInfo.crosslineId = 6;
 % GPostInvParam.initModel.segyInfo.t0 = 0;
 % GPostInvParam.initModel.segyFileName = 'D:\data\seismic data\geo-block\data\model\Imp_BGP_Init.sgy';
 
-GPostInvParam.initModel.segyInfo.inlineId = 48;
-GPostInvParam.postSeisData.segyInfo.crosslineId = 6;
-GPostInvParam.initModel.segyInfo.t0 = 0;
-GPostInvParam.initModel.segyFileName = 'C:\Users\binst\HRS projects\GEO_POST\model\modelall_Volume_PP_IMP.sgy';
+% GPostInvParam.initModel.segyInfo.t0 = 780;
+% GPostInvParam.initModel.segyFileName = 'C:\Users\binst\HRS projects\GEO_POST\inv_result\geoeast_impedance_sparse_impulse_bgp.sgy';
+
+% GPostInvParam.initModel.segyInfo.inlineId = 48;
+% GPostInvParam.postSeisData.segyInfo.crosslineId = 6;
+% GPostInvParam.initModel.segyInfo.t0 = 0;
+% GPostInvParam.initModel.segyFileName = 'C:\Users\binst\HRS projects\GEO_POST\model\modelall_Volume_PP_IMP.sgy';
 
 % segy information of poststack file
 GPostInvParam.postSeisData.segyInfo = GSegyInfo;
@@ -62,15 +65,57 @@ GPostInvParam.postSeisData.segyFileName = 'D:\data\seismic data\geo-block\data\s
 GPostInvParam.postSeisData.shiftSegyFileName = 'D:\data\seismic data\geo-block\data\seismic\phase_shift_90.sgy';
 GPostInvParam.dt = 1;                           
 GPostInvParam.isNormal = 1;                     % whether normalize
-% GPostInvParam.upNum = 130;                      
+GPostInvParam.upNum = 80;   
+% GPostInvParam.upNum = 80;  
 GPostInvParam.downNum = 50;      
-GPostInvParam.upNum = 80;     
-GPostInvParam.isSaveMode = 1;
 GPostInvParam.waveletFreq = 45;
+GPostInvParam.isParallel = 1;
+GPostInvParam.isSaveMode = 0;
+GPostInvParam.isReadMode = 0;
+
+GPostInvParam.bound.mode = 'based_on_init';
+GPostInvParam.bound.offset_init = 2500;
+
+GPostInvParam.indexOfTimeInWellData = 2;
 
 % load zeroPhaseWavelet.mat;
 % load ricker.mat;
-load rickerWavelet.mat;
+% load rickerWavelet.mat;
+% load extractedWavelet.mat;
+load extractedZeroPhaseWavelet.mat;
 GPostInvParam.wavelet = wavelet;
-parentPath = 'IP_DIC';
 
+%% set the options for showing the results 
+GShowProfileParam.isLegend = 0;
+GShowProfileParam.plotParam = GPlotParam;
+GShowProfileParam.rangeSeismic = [];
+% GShowProfileParam.showProfileFiltCoef = 1;
+GShowProfileParam.scaleFactor = 5;  % interpolate the inversion results avoid zigzag 
+   
+GShowProfileParam.showWellOffset = 1;
+GShowProfileParam.showWellFiltCoef = 0.4;
+load attColor.mat;
+load original_color.mat
+GShowProfileParam.dataColorTbl = original_color;
+GShowProfileParam.isColorReverse = 0;           % whether call filpud fcn to reverse the color table
+
+%% set the options of inversion process
+seisInvOptions = bsCreateSeisInv1DOptions();
+seisInvOptions.GBOptions.optAlgHandle = @bsOptQCG;          % using Quasi-Newton conjugate gradient optimizer
+seisInvOptions.GBOptions.display = 'off';
+seisInvOptions.GBOptions.optAlgParam.updateFlag = 'PR';     % PR formulation, could be also FR, HS, DY
+seisInvOptions.GBOptions.isSaveMiddleRes = false;           % Whether save the middle results
+
+seisInvOptions.addLowFreqConstraint = true;
+seisInvOptions.initRegParam = 0.5;
+seisInvOptions.searchRegParamFcn = [];
+
+%% set the options for training dictionary
+GTrainDICParam.sizeAtom = 60;
+GTrainDICParam.nAtom = 8000;
+GTrainDICParam.filtCoef= 0.5;
+
+%% set the options for sparse inversion
+GSparseInvParam.sparsity =1;
+GSparseInvParam.stride = 1;
+GSparseInvParam.isSparseRebuild = 1;
